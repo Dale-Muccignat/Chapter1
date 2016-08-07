ae8c59d312b565c73b60012fdfb33269bdfc443c
Dale Muccignat <dale.muccignat@my.jcu.edu.au>
2016-08-07 12:33:56 +1000

    Initial commit

---
+++ src/BookQuote.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Dale on 7/08/2016.
+ */
+import javax.swing.JOptionPane;
+public class BookQuote {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "In the beginning the Universe was created.\n" +
+                "This has made a lot of people very angry and has been widely regarded as a bad move.");
+    }
+}
---
+++ src/BookQuoteInfo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Dale on 7/08/2016.
+ */
+import javax.swing.JOptionPane;
+public class BookQuoteInfo {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Book: The Hitchhiker's Guide to the Universe\n" +
+        "Author: Douglas Adams\n" + "Publication date: 1979");
+    }
+}
